d5327c9f029bb2e8dc60ff409e8e86f25cfd2dd2
Julian Tescher <virulent@gmail.com>
2013-01-22 14:53:03 -0800

    Remove ruby 1.9 specific syntax from specs

--- spec/image_optimizer/jpeg_optimizer_spec.rb
+++ spec/image_optimizer/jpeg_optimizer_spec.rb
@@ -6,13 +6,13 @@ describe ImageOptimizer::JPEGOptimizer do
     let(:jpeg_optimizer) { ImageOptimizer::JPEGOptimizer.new(file_path) }
 
     it 'optimizes the jpeg' do
-      jpeg_optimizer.stub(jpeg_optimizer_present?: true)
+      jpeg_optimizer.stub(:jpeg_optimizer_present? => true)
       jpeg_optimizer.should_receive(:system).with("jpegoptim -f --strip-all #{jpeg_optimizer.path}")
       jpeg_optimizer.optimize
     end
 
     it 'warns the user if the jpeg optimizing utility is not installed' do
-      jpeg_optimizer.stub(jpeg_optimizer_present?: false)
+      jpeg_optimizer.stub(:jpeg_optimizer_present? => false)
       jpeg_optimizer.should_receive(:warn).with('Attempting to optimize a jpeg without jpegoptim installed. Skipping...')
       jpeg_optimizer.optimize
     end
--- spec/image_optimizer/png_optimizer_spec.rb
+++ spec/image_optimizer/png_optimizer_spec.rb
@@ -6,13 +6,13 @@ describe ImageOptimizer::PNGOptimizer do
     let(:png_optimizer) { ImageOptimizer::PNGOptimizer.new(file_path) }
 
     it 'optimizes the png' do
-      png_optimizer.stub(png_optimizer_present?: true)
+      png_optimizer.stub(:png_optimizer_present? => true)
       png_optimizer.should_receive(:system).with("optipng -o7 #{png_optimizer.path}")
       png_optimizer.optimize
     end
 
     it 'warns the user if the png optimizing utility is not installed' do
-      png_optimizer.stub(png_optimizer_present?: false)
+      png_optimizer.stub(:png_optimizer_present? => false)
       png_optimizer.should_receive(:warn).with('Attempting to optimize a png without optipng installed. Skipping...')
       png_optimizer.optimize
     end
